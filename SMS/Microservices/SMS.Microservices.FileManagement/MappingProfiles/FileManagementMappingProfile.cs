ca2b90365b2aa33922aff87f8a2a0ed6c1eb26ef
0ankit0 <ankitpaudel.1373@gmail.com>
2025-08-26 07:29:16 +0545

    Refactor file management for Azure Blob Storage support
    - Updated `FileManagementDbContext` to rename `FileStorage` to `FileStorages` and removed timestamp handling methods.
    - Added new file upload and download endpoints in `FileEndpoints.cs` for Azure Blob Storage.
    - Modified `FileManagementMappingProfile` to map `Id` and `UploadedByUserId`.
    - Changed `FileStorage` class to use `Id` as the primary key and updated user ID property.
    - Enhanced service configuration in `Program.cs` for Azure Blob Storage and PostgreSQL.
    - Updated project dependencies in `SMS.Microservices.FileManagement.csproj`.
    - Adjusted logging levels and added connection strings in `appsettings` files.

--- SMS/Microservices/SMS.Microservices.FileManagement/MappingProfiles/FileManagementMappingProfile.cs
+++ SMS/Microservices/SMS.Microservices.FileManagement/MappingProfiles/FileManagementMappingProfile.cs
@@ -1,6 +1,6 @@
 using AutoMapper;
-using SMS.Data.Models;
 using SMS.Contracts.FileStorage;
+using SMS.Microservices.FileManagement.Models;
 
 namespace SMS.Microservices.FileManagement.MappingProfiles;
 
@@ -9,6 +9,7 @@ public class FileManagementMappingProfile : Profile
     public FileManagementMappingProfile()
     {
         CreateMap<FileStorage, FileStorageResponse>()
-            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.ExternalId));
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.UploadedByUserExternalId, opt => opt.MapFrom(src => src.UploadedByUserId));
     }
 }
